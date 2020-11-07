99a03c41138c46d15d8f086a47deb3b806eabb1c
Patrick Malatesta <malatep@amazon.com>
2020-11-07 16:24:59 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-1",
+      BUCKET: "serverless-appnotes-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://rf443k1y11.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_Id9VDe5jE",
+      APP_CLIENT_ID: "7l4rkp1fpc9g2hhvdltcu2vnpp",
+      IDENTITY_POOL_ID: "eu-west-1:6d1a0cf0-34d2-431c-9c13-ba53cbf4663f"
+    }
+  };
